f2157be7009837c5920579efaabf5abff4d81460
Olivier <herculepoirot75@hotmail.fr>
2024-09-09 13:24:37 +0200

    first commit

---
+++ FrontEnd/script/script.js
@@ -0,0 +1,17 @@
+fetch("http://localhost:5678/api/works")
+  .then((response) => response.json())
+  .then((data) => {
+    const gallery = document.querySelector(".gallery");
+    data.forEach((work) => {
+      const listPhotos = work.imageUrl;
+      const nomPhotos = work.title;
+      const img = document.createElement("img");
+      img.src = listPhotos;
+      img.alt = "image";
+      gallery.appendChild(img);
+      gallery.insertAdjacentHTML("beforeend", nomPhotos);
+      console.log(work.imageUrl);
+    });
+  })
+  .catch((error) => console.error(error));
+//ajouter picture
