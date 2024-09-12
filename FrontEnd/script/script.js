f575b59b8ad6e2efc7b2a1cbd8570e28012a271a
Olivier <herculepoirot75@hotmail.fr>
2024-09-12 19:43:34 +0200

    switch

--- FrontEnd/script/script.js
+++ FrontEnd/script/script.js
@@ -1,3 +1,4 @@
+const filters = document.getElementById("filters");
 const gallery = document.querySelector(".gallery");
 
 fetch("http://localhost:5678/api/works")
@@ -24,256 +25,136 @@ fetch("http://localhost:5678/api/works")
 //---------------------------------------------------------------
 //-----------intégration menu de catégories----------------------
 //---------------------------------------------------------------
+let categories = []; // création d'un tableau vide qui se remplira dynamiquement
 
-const projets = document.getElementById("projets");
-const btnTous = document.getElementById("1");
-const btnObjets = document.getElementById("2");
-const btnAppartements = document.getElementById("3");
-const btnHotelsEtRestaurants = document.getElementById("4");
-
-async function getObjets() {
-  // création fonction asynchrone pour la démarrer en attendant le retour de la promesse qui retourne le tableau "category"
-  const tri1 = await fetch("http://localhost:5678/api/categories"); // attente de la résolution de la promesse qu(on passe en const "tri1")
-  return await tri1.json(); // on convertit la const "a" en format .json pour être lisible par l'ordi avec await
-}
-getObjets();
-
-//---------------------------------------------------------------
-//-----------------------TOUS------------------------------------
-//---------------------------------------------------------------
-
-//ajouter transitions, autres photos, noms
-btnTous.addEventListener("click", () => {
-  gallery.innerHTML = "";
-  fetch("http://localhost:5678/api/works")
-    .then((response) => response.json())
-    .then((data) => {
-      data.forEach((works) => {
-        const listPhotos = works.imageUrl;
-        const nomPhotos = works.title;
-        const figure = document.createElement("figure");
-        const img = document.createElement("img");
-        const figCaption = document.createElement("figcaption");
-        img.src = listPhotos;
-        img.alt = "image";
-        figure.appendChild(img);
-        figure.appendChild(figCaption);
-        gallery.appendChild(figure);
-        figure.insertAdjacentHTML("beforeend", nomPhotos);
-      });
+async function getCategories() {
+  const btnTous = document.createElement("button");
+  btnTous.innerText = "Tous";
+  filters.appendChild(btnTous);
+  try {
+    const response = await fetch("http://localhost:5678/api/categories");
+    const data = await response.json();
+    data.forEach((element) => {
+      const button = document.createElement("button");
+      button.innerText = element.name;
+      filters.appendChild(button);
     });
-});
-//---------------------------------------------------------------
-//-----------------------OBJETS----------------------------------
-//---------------------------------------------------------------
-
-btnObjets.addEventListener("click", (works) => {
-  async function afficherObjets() {
-    const arrayObjets = await getObjets();
-    const figure = document.createElement("figure");
-    const img = document.createElement("img");
-    const figCaption = document.createElement("figcaption");
-    fetch("http://localhost:5678/api/works").then((response) => response.json);
-    figCaption.textContent = works.name;
-    gallery.innerHTML = "";
-    figure.classList.add("img");
-    img.src = "http://localhost:5678/images/abajour-tahina1651286843956.png";
-    figure.appendChild(img);
-    figure.appendChild(figCaption);
-    gallery.appendChild(figure);
-    //console.log(arrayObjets);
-  }
-  afficherObjets();
-});
-//---------------------------------------------------------------
-//-----------------------APPARTEMENTS----------------------------
-//---------------------------------------------------------------
-
-btnAppartements.addEventListener("click", (works) => {
-  async function afficherAppartements() {
-    const arrayObjets = await getObjets();
-    const figure = document.createElement("figure");
-    const img = document.createElement("img");
-    const figCaption = document.createElement("figcaption");
-    fetch("http://localhost:5678/api/works").then((response) => response.json);
-    figCaption.textContent = works.name;
-    gallery.innerHTML = "";
-    figure.classList.add("img");
-    img.src =
-      "http://localhost:5678/images/appartement-paris-v1651287270508.png";
-    figure.appendChild(img);
-    figure.appendChild(figCaption);
-    gallery.appendChild(figure);
-    // console.log(arrayObjets);
-  }
-  afficherAppartements();
-});
-//---------------------------------------------------------------
-//-----------------------HOTELS ET RESTAURANTS-------------------
-//---------------------------------------------------------------
-
-btnHotelsEtRestaurants.addEventListener("click", (works) => {
-  async function afficherHotelsEtRestaurants() {
-    const arrayObjets = await getObjets();
-    const figure = document.createElement("figure");
-    const img = document.createElement("img");
-    const figCaption = document.createElement("figcaption");
-    fetch("http://localhost:5678/api/works").then((response) => response.json);
-    figCaption.textContent = works.name;
-    gallery.innerHTML = "";
-    figure.classList.add("img");
-    img.src =
-      "http://localhost:5678/images/restaurant-sushisen-londres1651287319271.png";
-    figure.appendChild(img);
-    figure.appendChild(figCaption);
-    gallery.appendChild(figure);
-    //console.log(arrayObjets);
-  }
-  afficherHotelsEtRestaurants();
-});
-
+  } catch {}
+}
+getCategories();
 //---------------------------------------------------------------
 //--------------------------LOGIN--------------------------------
 //---------------------------------------------------------------
 document.getElementById("btn-login").addEventListener("click", () => {
   window.location.href = "./LogIn/login.html";
 });
-/*
-btnObjets.addEventListener("click", () => {
-  async function afficherObjets() {
-    const arrayObjets = await getObjets();
-    console.log(arrayObjets); //OK
-    arrayObjets.forEach((works) => {
-      const listPhotos = works.imageUrl;
-      const figure = document.createElement("figure");
-      const img = document.createElement("img");
-      const figCaption = document.createElement("figcaption");
-      fetch("http://localhost:5678/api/works").then(
-        (response) => response.json
-      );
-      console.log(works);
-      //img.src = listPhotos; // utile ?
-      figCaption.textContent = works.id;
-      //
-      //code ci-dessous pas bon
-      //
-      gallery.innerHTML = "";
-      figure.classList.add("img");
-      img.src = "http://localhost:5678/images/abajour-tahina1651286843956.png";
-      //
-      //code ci-dessus pas bon
-      //il faut un appel dynamique de l'image en fn du nom objet/restau/hôtel....
-      // genre if(category.name="objets") {img.src = (category.name`${works.category.imageUrl}`)}
-      // genre if(category.name="restaurants") {img.src = (category.name`${works.category.imageUrl}`)}
-      // genre if(category.name="hotels) {img.src = (category.name`${works.category.imageUrl}`)}
-      //
-      //
-      figure.appendChild(img);
-      figure.appendChild(figCaption);
-      gallery.appendChild(figure);
-      console.log(figure);
-      //console.log(img);
-      //console.log(figCaption);
-    });
-  }
-  afficherObjets();
-});
-*/
 //
 //
 //
 //
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-/*
-btnObjets.addEventListener("click", () => {
-  const arrayObjets = getObjets();
-  for (i = 0; i < arrayObjets.lenght; i++) {
-    afficherObjets(i);
-  }
-});
+// const projets = document.getElementById("projets");
+// const btnTous = document.getElementById("1");
+// const btnObjets = document.getElementById("2");
+// const btnAppartements = document.getElementById("3");
+// const btnHotelsEtRestaurants = document.getElementById("4");
 
-// btnObjets.addEventListener("click", () => {
-//   fetch("http://localhost:5678/api/categories")
-//     .then((response) => response.json)
+// async function getObjets() {
+//   // création fonction asynchrone pour la démarrer en attendant le retour de la promesse qui retourne le tableau "category"
+//   const tri1 = await fetch("http://localhost:5678/api/categories"); // attente de la résolution de la promesse qu(on passe en const "tri1")
+//   return await tri1.json(); // on convertit la const "a" en format .json pour être lisible par l'ordi avec await
+// }
+// getObjets();
+
+// //---------------------------------------------------------------
+// //-----------------------TOUS------------------------------------
+// //---------------------------------------------------------------
+
+// //ajouter transitions, autres photos, noms
+// btnTous.addEventListener("click", () => {
+//   gallery.innerHTML = "";
+//   fetch("http://localhost:5678/api/works")
+//     .then((response) => response.json())
 //     .then((data) => {
-//       const gallery = document.querySelector(".gallery");
-//       data.forEach((category) => {
-//         const listObjets = category.imageUrl;
-//         const nomPhotos = category.name;
+//       data.forEach((works) => {
+//         const listPhotos = works.imageUrl;
+//         const nomPhotos = works.title;
 //         const figure = document.createElement("figure");
 //         const img = document.createElement("img");
 //         const figCaption = document.createElement("figcaption");
 //         img.src = listPhotos;
 //         img.alt = "image";
-//         figure.appendChild(listObjets);
 //         figure.appendChild(img);
 //         figure.appendChild(figCaption);
 //         gallery.appendChild(figure);
 //         figure.insertAdjacentHTML("beforeend", nomPhotos);
 //       });
 //     });
+// });
+// //---------------------------------------------------------------
+// //-----------------------OBJETS----------------------------------
+// //---------------------------------------------------------------
 
-//   btnObjets.addEventListener("click", () => {
-//     const nomCategorieRecherche = "objets";
-//     const objetsFiltres = work.category.name.filter(
-//       (objet) => work.category.name === nomCategorieRecherche
-//     );
-//     objetsFiltres.forEach((objet) => {
-//       console.log(`id: ${objet.id}`);
-//  });
-
-//console.log(work.category.name);
-//   objets.addEventListener("click", () => {
-//     const projetsObjets = work.category;
-//     gallery.innerHTML = projetsObjets;
-//     console.log(work.category.name);
-//   });
-//console.log(work.category.name);
-//   });
-//   })
-// .catch((error) => console.error(error));
-/*
-
-//--------------------------------------------------
-//--------------------------------------------------
-//--------------------------------------------------
-// tentative de bouton de tri
-
-//   function filterImages() {
-//     for (i = 0; i < work.category.name; i++) {
-//       const imagesContainer = document.querySelector(".gallery");
-//       imagesContainer.innerHTML = ""; // vide le container des images
-//       imagesContainer.forEach((image) => {
-//         if (image.gallery.category.name === "objets") {
-//           const imagesElements = document.createElement("img");
-//           imagesElements.src = work.category.imageUrl;
-//           imagesElements.alt = `work.category.name${filterImages}`;
-//           imagesElements.classList.add("image-container");
-//           imagesElements.style.display = "Block";
-//           imagesContainer.appendChild(imagesElements);
-//         }
-//       });
-//     }
-//     filterImages(i);
+// btnObjets.addEventListener("click", (works) => {
+//   async function afficherObjets() {
+//     const arrayObjets = await getObjets();
+//     const figure = document.createElement("figure");
+//     const img = document.createElement("img");
+//     const figCaption = document.createElement("figcaption");
+//     fetch("http://localhost:5678/api/works").then((response) => response.json);
+//     figCaption.textContent = works.name;
+//     gallery.innerHTML = "";
+//     figure.classList.add("img");
+//     img.src = "http://localhost:5678/images/abajour-tahina1651286843956.png";
+//     figure.appendChild(img);
+//     figure.appendChild(figCaption);
+//     gallery.appendChild(figure);
+//     //console.log(arrayObjets);
 //   }
-//identifier les images pour accéder aux !==tes catégories
+//   afficherObjets();
+// });
+// //---------------------------------------------------------------
+// //-----------------------APPARTEMENTS----------------------------
+// //---------------------------------------------------------------
 
-/*
-menuDeCategories.addEventListener("click", () => {
-  const tri = Array.from(work.category.name);
-  return work.category.name;
-  console.log(tri);
-});
+// btnAppartements.addEventListener("click", (works) => {
+//   async function afficherAppartements() {
+//     const arrayObjets = await getObjets();
+//     const figure = document.createElement("figure");
+//     const img = document.createElement("img");
+//     const figCaption = document.createElement("figcaption");
+//     fetch("http://localhost:5678/api/works").then((response) => response.json);
+//     figCaption.textContent = works.name;
+//     gallery.innerHTML = "";
+//     figure.classList.add("img");
+//     img.src =
+//       "http://localhost:5678/images/appartement-paris-v1651287270508.png";
+//     figure.appendChild(img);
+//     figure.appendChild(figCaption);
+//     gallery.appendChild(figure);
+//     // console.log(arrayObjets);
+//   }
+//   afficherAppartements();
+// });
+// //---------------------------------------------------------------
+// //-----------------------HOTELS ET RESTAURANTS-------------------
+// //---------------------------------------------------------------
 
-*/
+// btnHotelsEtRestaurants.addEventListener("click", (works) => {
+//   async function afficherHotelsEtRestaurants() {
+//     const arrayObjets = await getObjets();
+//     const figure = document.createElement("figure");
+//     const img = document.createElement("img");
+//     const figCaption = document.createElement("figcaption");
+//     fetch("http://localhost:5678/api/works").then((response) => response.json);
+//     figCaption.textContent = works.name;
+//     gallery.innerHTML = "";
+//     figure.classList.add("img");
+//     img.src =
+//       "http://localhost:5678/images/restaurant-sushisen-londres1651287319271.png";
+//     figure.appendChild(img);
+//     figure.appendChild(figCaption);
+//     gallery.appendChild(figure);
+//     //console.log(arrayObjets);
+//   }
+//   afficherHotelsEtRestaurants();
+// });
