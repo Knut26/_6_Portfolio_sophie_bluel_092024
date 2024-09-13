426677454c2ff1a6ce903f4b9526dcf38f0a77d1
Olivier <herculepoirot75@hotmail.fr>
2024-09-13 11:43:48 +0200

    buttons

--- FrontEnd/script/script.js
+++ FrontEnd/script/script.js
@@ -39,25 +39,13 @@ async function getCategories() {
       button.innerText = element.name;
       filters.appendChild(button);
       button.addEventListener("click", () => {
-        fetch("http://localhost:5678/api/works");
-        const figure = document.createElement("figure");
-        const img = document.createElement("img");
-        const figCaption = document.createElement("figcaption");
-        gallery.innerHTML = "";
-        img.src =
-          "http://localhost:5678/images/abajour-tahina1651286843956.png";
-        figure.classList.add("img");
-        figCaption.textContent = element.name;
-        figure.appendChild(img);
-        figure.appendChild(figCaption);
-        gallery.appendChild(figure);
-        figure.insertAdjacentHTML("beforeend", nomPhotos);
-        console.log("ca marche");
+        console.log(response);
       });
     });
   } catch {}
 }
 getCategories();
+
 //---------------------------------------------------------------
 //--------------------------LOGIN--------------------------------
 //---------------------------------------------------------------
