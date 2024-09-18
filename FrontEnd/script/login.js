39ae290dad928ac0806e9aae813c49c66145fe60
Olivier <herculepoirot75@hotmail.fr>
2024-09-18 09:47:18 +0200

    comm log in ok

--- FrontEnd/script/login.js
+++ FrontEnd/script/login.js
@@ -1,22 +1,24 @@
 //---------------------------------------------------------------
 //--------------------------LOGIN--------------------------------
 //---------------------------------------------------------------
-const logInForm = document.getElementById("logIn-form");
+const logInForm = document.getElementById("logIn-form"); //récup du formulaire complet
 logInForm.addEventListener("submit", (event) => {
+  //création d'un event
   event.preventDefault(); //empêche l'envoi du formulaire
 
-  const validEmail = "sophie.bluel@test.tld";
-  const validPassword = "S0phie";
+  const email = document.getElementById("email").value; //création const email avec une valeur entrée par l'utilisateur
+  const password = document.getElementById("password").value; //création const password avec une valeur entrée par l'utilisateur
 
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+  const validEmail = "sophie.bluel@test.tld"; //const email valide
+  const validPassword = "S0phie"; //const password valide
 
   if (email === validEmail && password === validPassword) {
+    //condition si l'ensemble des 2 const sont valides...
     alert(
-      "Bienvenue admin, vous allez être redirigé vers la page d'accueil..."
+      "Bienvenue admin, vous allez être redirigé vers la page d'accueil..." //message popup "ok"
     );
-    window.location.href = "../index.html";
+    window.location.href = "../index.html"; //et redirection vers page index.html
   } else {
-    alert("Veuillez rentrer des identifiants valides");
+    alert("Veuillez rentrer des identifiants valides"); //sinon popup message d'erreur
   }
 });
