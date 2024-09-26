c36f98cdbc87de5313dea4b5b7a809eea05c47a3
Olivier <herculepoirot75@hotmail.fr>
2024-09-26 11:21:12 +0200

    test ajout trash can

--- FrontEnd/script/modale.js
+++ FrontEnd/script/modale.js
@@ -1,19 +1,44 @@
-const openModal = function (e) {
-  e.preventDefault();
-  const target = document.querySelector(e.target.getAttribute("href"));
-  target.style.display = null;
-  target.removeAttribute("aria-hidden");
-  target.setAttribute("aria-modal", "true");
-};
+// const openModal = function (e) {
+//   e.preventDefault();
+//   const target = document.querySelector(e.target.getAttribute("href"));
+//   target.style.display = null;
+//   target.removeAttribute("aria-hidden");
+//   target.setAttribute("aria-modal", "true");
+// };
 
-const linkModale = document.getElementById("link-modale");
+// const linkModale = document.getElementById("link-modale");
 
-linkModale.forEach((a) => {
-  a.addEventListener("click", openModal);
-  //création div test
-  const test = document.createElement("div");
-  test.className("test");
-  test.innerHTML = "ceci est un test";
-  linkModale.appendChild(test);
-  //création div test
-});
+// linkModale.forEach((a) => {
+//   a.addEventListener("click", openModal);
+//   //création div test
+//   const test = document.createElement("div");
+//   test.className("test");
+//   test.innerHTML = "ceci est un test";
+//   linkModale.appendChild(test);
+//   //création div test
+// });
+const modale1 = document.querySelector(".modale-photos");
+
+fetch("http://localhost:5678/api/works")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    for (i = 0; i < data.length; i++) {
+      const img = document.createElement("img");
+      img.className = "modale-img";
+      const imageUrl = data[i].imageUrl;
+      img.src = imageUrl;
+      modale1.appendChild(img);
+      //test ajout trash can
+      data.forEach(displayTrash);
+
+      function displayTrash() {
+        const p = document.createElement("p");
+        p.innerHTML = '<i class="fa-solid fa-trash-can trash"></i>';
+        p.className = "trash";
+        img.appendChild(p);
+      }
+    }
+  })
+
+  .catch((error) => console.error(error));
