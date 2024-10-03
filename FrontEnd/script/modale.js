f4d332fa5ecf6fed8ac9419eb2036d165b3a79e0
Olivier <herculepoirot75@hotmail.fr>
2024-10-03 11:21:34 +0200

    bearer

--- FrontEnd/script/modale.js
+++ FrontEnd/script/modale.js
@@ -1,24 +1,3 @@
-// const openModal = function (e) {
-//   e.preventDefault();
-//   const target = document.querySelector(e.target.getAttribute("href"));
-//   target.style.display = null;
-//   target.removeAttribute("aria-hidden");
-//   target.setAttribute("aria-modal", "true");
-// };
-
-// const linkModale = document.getElementById("link-modale");
-
-// linkModale.forEach((a) => {
-//   a.addEventListener("click", openModal);
-//   //création div test
-//   const test = document.createElement("div");
-//   test.className("test");
-//   test.innerHTML = "ceci est un test";
-//   linkModale.appendChild(test);
-//   //création div test
-// });
-
-//
 //---------------------------------------------------------------
 //----------------------intégration modale1----------------------
 //---------------------------------------------------------------
@@ -55,7 +34,7 @@ function fetchingModale1() {
             method: "DELETE",
             headers: {
               "Content-Type": "application/json",
-              authorization: `Bearer ${authToken}`,
+              authorization: "Bearer " + authToken,
             },
           };
           fetch(fetching, options).then((response) =>
