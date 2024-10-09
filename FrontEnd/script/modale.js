751409a00c1b6e696d0e3c704540c154d8f3e1f7
Olivier <herculepoirot75@hotmail.fr>
2024-10-09 11:55:51 +0200

    modif pour modale2 en HTML ok

--- FrontEnd/script/modale.js
+++ FrontEnd/script/modale.js
@@ -1,8 +1,8 @@
 //
 //
-//------------------------------------------------------
-//-------------intégration modale1----------------------
-//------------------------------------------------------
+//--------------------------------------------------
+//---------intégration modale1----------------------
+//--------------------------------------------------
 //
 //
 const modale1 = document.querySelector(".modale-photos");
@@ -22,7 +22,7 @@ function fetchingModale1() {
         const imageUrl = data[i].imageUrl;
         img.src = imageUrl;
 
-        //--------------MODALE1 : DELETE-----------------
+        //-----------MODALE1 : DELETE-----------------
 
         const trashCan = document.createElement("p"); //
         trashCan.innerHTML = '<i class="fa-solid fa-trash-can trash"></i>';
@@ -60,262 +60,64 @@ function fetchingModale1() {
     });
 }
 
+//
+//
+//-----------------------------------------------------
+//------------intégration modale2----------------------
+//-----------------------------------------------------
+//
+//
 fetchingModale1();
-//
-//
-//---------------------------------------------------------
-//----------------intégration modale2----------------------
-//---------------------------------------------------------
-//
-//
 
 const ajoutPhoto = document.getElementById("ajout-photo");
+const modale2 = document.getElementById("modale2");
 
-ajoutPhoto.addEventListener("click", function (event) {
-  event.preventDefault();
-  modale1.innerHTML = "";
-  //////////////////////////////WIP//////////////////////////////////
-  //////////////////////////////WIP//////////////////////////////////
-  //////////////////////////////WIP//////////////////////////////////
-  ajoutPhoto.addEventListener("click", function (event) {
-    event.preventDefault();
-    const validBox = document.createElement("div");
-    validBox.className = "valid-box";
-    validBox.innerHTML = "photo validée !";
-    select.appendChild(validBox);
+ajoutPhoto.addEventListener("click", () => {
+  modale2.style.display = "flex";
+});
+
+const backArrow = document.querySelector(".fa-arrow-left");
+
+backArrow.addEventListener("click", () => {
+  modale2.style.display = "none";
+});
+
+function ajoutOption(selectElement, value, text) {
+  let option = document.createElement("option");
+  option.value = value;
+  option.textContent = text;
+  selectElement.appendChild(option);
+}
+
+const select = document.getElementById("options");
+const form2 = document.querySelector(".form2");
+
+fetch("http://localhost:5678/api/categories") //fetch les categoryId
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    for (i = 0; i < data.length; i++) {
+      const choice = data[i].name; //on récup les noms des catégories
+      ajoutOption(select, "options[i]", choice); //on créé les choix du menu déroulant
+    }
   });
-  //////////////////////////////WIP//////////////////////////////////
-  //////////////////////////////WIP//////////////////////////////////
-  //////////////////////////////WIP//////////////////////////////////
-  //------------------flèche backpage-------------------------
-  const backArrow = document.createElement("a");
-  backArrow.href = "#"; /////////???????????????????????????????????
-  backArrow.innerHTML =
-    '<p><i class="fa-solid fa-arrow-left back-arrow"></i></p>';
-  backArrow.style.color = "black";
-  modale1.appendChild(backArrow);
-  //--------------addevent sur backArrow--------------------
-  //////////////////////////////WIP//////////////////////////////////
-  //////////////////////////////WIP//////////////////////////////////
-  //////////////////////////////WIP//////////////////////////////////
-  backArrow.addEventListener("click", () => {
-    modaleWrapper.innerHTML = "Galerie photo";
-    close.style.display = "block";
-    close.innerHTML = '<p><i class="fas fa-regular fa-xmark"></i></p>';
-    modalePhotos.innerHTML = "";
-    modalePhotos.style.display = "grid";
-    addPhotoPictureContainer.innerHTML = "";
-    addPhotoPicture.innerHTML = "";
-    addPhotoPictureButton.innerHTML = "";
-    textPhoto.innerHTML = "";
-    form1.innerHTML = "";
-    formNom.innerHTML = "";
-    labelNom.innerHTML = "";
-    inputNom.innerHTML = "";
-    form2.innerHTML = "";
-    formNom2.innerHTML = "";
-    labelNom2.innerHTML = "";
-    select.innerHTML = "";
-    ajoutPhoto.innerHTML = "Ajouter une photo";
-    ajoutPhoto.style.backgroundColor = "#1d6154";
+ajoutOption(select, "option1", "");
 
-    fetchingModale1();
-  });
-  //////////////////////////////WIP//////////////////////////////////
-  //////////////////////////////WIP//////////////////////////////////
-  //////////////////////////////WIP//////////////////////////////////
+//---------fermeture modale 2------------------
 
-  //------------------croix fermeture modale-------------------------
-  const close = document.createElement("a");
-  close.href = "#";
-  close.innerHTML = '<p><i class="fas fa-regular fa-xmark"></i></p>';
-  close.style.color = "black";
-  close.id = "comeBackToIndex";
-  modale1.appendChild(close);
+const visibleModale = document.querySelector(".modale-visible");
+const overlay = document.getElementById("overlay");
+const comeBackToIndex = document.getElementById("comeBackToIndex");
 
-  //------------------changement titre-------------------------
-  const modaleWrapper = document.querySelector(".modale-wrapper");
-  modaleWrapper.innerHTML = "Ajout photo";
+comeBackToIndex.addEventListener("click", () => {
+  visibleModale.style.display = "none";
+  modale2.style.display = "none";
+});
 
-  //------------------div container-------------------------
-  const addPhotoPictureContainer = document.createElement("div");
-  addPhotoPictureContainer.className = "add-photo-picture-container";
-  modale1.appendChild(addPhotoPictureContainer);
-
-  //------------------font awesome photo-------------------------
-  const addPhotoPicture = document.createElement("p");
-  addPhotoPicture.href = "#";
-  addPhotoPicture.innerHTML =
-    '<i class="fa-regular fa-image add-photo-picture"></i>';
-  addPhotoPictureContainer.appendChild(addPhotoPicture);
-
-  //------------------ajout button-------------------------
-  const addPhotoPictureButton = document.createElement("button");
-  addPhotoPictureButton.className = "add-photo-picture-button";
-  addPhotoPictureButton.innerHTML = "+ Ajouter photo";
-  addPhotoPictureContainer.appendChild(addPhotoPictureButton);
-  //--------------addevent sur + ajouter photo--------------------
-  //////////////////////////////WIP//////////////////////////////////
-  //////////////////////////////WIP//////////////////////////////////
-  //////////////////////////////WIP//////////////////////////////////
-  // addPhotoPictureButton.addEventListener("click", () => {
-  //   let options = {
-  //     method: "GET",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //       authorization: "Bearer " + authToken,
-  //     },
-  //   };
-  //   fetch(fetching, options).then((response) => console.log(response.status));
-  //   console.log(data);
-  //   console.log(authToken);
-  // });
-  //////////////////////////////WIP//////////////////////////////////
-  //////////////////////////////WIP//////////////////////////////////
-  //////////////////////////////WIP//////////////////////////////////
-
-  //------------------ajout texte-------------------------
-  const textPhoto = document.createElement("p");
-  textPhoto.className = "text-photo";
-  textPhoto.innerHTML = "jpg, png : 4mo max";
-  addPhotoPictureContainer.appendChild(textPhoto);
-
-  //------------------formulaire 1-------------------------
-  const form1 = document.createElement("div");
-  form1.className = "form1";
-  form1.style.display = "flex";
-  form1.style.flexDirection = "column";
-
-  const formNom = document.createElement("form");
-  formNom.action = "#";
-  formNom.method = "post";
-  form1.appendChild(formNom);
-
-  const labelNom = document.createElement("label");
-  labelNom.className = "label-nom1";
-  labelNom.innerHTML = "Titre";
-  labelNom.for = "name";
-  form1.appendChild(labelNom);
-
-  const inputNom = document.createElement("input");
-  inputNom.className = "input-nom1";
-  inputNom.type = "text";
-  inputNom.name = "name";
-  inputNom.id = "name";
-  form1.appendChild(inputNom);
-
-  modale1.appendChild(form1);
-
-  //------------------formulaire 2-------------------------
-  const form2 = document.createElement("div");
-  form2.className = "form2";
-  form2.style.display = "flex";
-  form2.style.flexDirection = "column";
-
-  const formNom2 = document.createElement("form");
-  formNom2.action = "#";
-  formNom2.method = "post";
-  form2.appendChild(formNom2);
-
-  const labelNom2 = document.createElement("options");
-  labelNom2.className = "label-nom2";
-  labelNom2.htmlFor = "options";
-  labelNom2.innerHTML = "Catégorie";
-  labelNom2.for = "options";
-  form2.appendChild(labelNom2);
-
-  //----------------avec menu déroulant---------------------
-  const select = document.createElement("select");
-  select.className = "select";
-  select.id = "options";
-  select.name = "options";
-  form2.appendChild(select);
-
-  function ajoutOption(selectElement, value, text) {
-    let option = document.createElement("option");
-    option.value = value;
-    option.textContent = text;
-    selectElement.appendChild(option);
-  }
-
-  fetch("http://localhost:5678/api/categories") //fetch les categoryId
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      for (i = 0; i < data.length; i++) {
-        const choice = data[i].name; //on récup les noms des catégories
-        ajoutOption(select, "options[i]", choice); //on créé les choix du menu déroulant
-      }
-    });
-  ajoutOption(select, "option1", ""); // avec un premier choix vide
-
-  modale1.appendChild(form2);
-
-  //------------------bouton valider-------------------------
-
-  ajoutPhoto.innerHTML = "valider";
-  ajoutPhoto.style.backgroundColor = "rgb(145, 154, 157)";
-  //--------------addevent sur valider--------------------
-  //////////////////////////////WIP//////////////////////////////////
-  //////////////////////////////WIP//////////////////////////////////
-  //////////////////////////////WIP//////////////////////////////////
-  // ajoutPhoto.addEventListener("click", () => {
-  // let options = {
-  //   method: "POST",
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //     authorization: "Bearer " + authToken,
-  //   },
-  // };
-  // fetch(fetching, options).then((response) => console.log(response.status));
-
-  //message erreur si les 2 champs ne sont pas remplis
-  //   if (form1 === "" || form2 === "") {
-  //     let errorBox2 = document.getElementById("errorBox2");
-  //     // if (errorBox2) {
-  //     errorBox2.remove();
-  //   }
-  //   errorBox2 = document.createElement("div"); //crée une div
-  //   errorBox2.className = "error-box2"; //
-  //   errorBox2.id = "errorBox2";
-  //   errorBox2.innerHTML = "Veuillez remplir les 2 champs de saisie";
-  //   const select = document.querySelector(".select");
-  //   select.className = "select";
-  //   select.id = "options";
-  //   select.name = "options";
-  //   form2.appendChild(select);
-  //   select.appendChild(errorBox2);
-  //   modale1.appendChild(form2);
-  //   console.log(errorBox2);
-  //   // }
-  // });
-  //////////////////////////////WIP//////////////////////////////////
-  //////////////////////////////WIP//////////////////////////////////
-  //////////////////////////////WIP//////////////////////////////////
-
-  //------------------modif div des formulaires-------------------------
-  const modalePhotos = document.querySelector(".modale-photos");
-  modalePhotos.style.display = "flex";
-  modalePhotos.style.flexDirection = "column";
-  modalePhotos.style.alignItems = "center";
-  modalePhotos.style.marginTop = "10px";
-  modalePhotos.style.marginBottom = "10px";
-
-  //------------------fermeture modale 2-------------------------
-
-  const visibleModale = document.querySelector(".modale-visible");
-  const overlay = document.getElementById("overlay");
-  const comeBackToIndex = document.getElementById("comeBackToIndex");
-
-  comeBackToIndex.addEventListener("click", function (event) {
-    event.preventDefault();
-    visibleModale.style.display = "none";
-  });
-
-  overlay.addEventListener("click", () => {
-    visibleModale.style.display = "none";
-    overlay.style.display = "none";
-  });
+overlay.addEventListener("click", () => {
+  visibleModale.style.display = "none";
+  modale2.style.display = "none";
+  overlay.style.display = "none";
 });
 
 //-------------------------------------------
